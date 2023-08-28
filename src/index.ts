1c15e0c7ab82c242e7dc791ba2077a47875dbdcd
Danny Yassine <dannyyassine@gmail.com>
2023-08-28 02:11:37 +0000

    create main

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+
+// routing components
+export { Route } from './Route';
+
+// jsx rendering
+export { render } from './jsx';
+
+// observables
+export { observable } from './Observable';
+
+// all decorators
+export * from './decorators';
+
+// base class that contains all of the above
+export { BaseView } from './BaseElement';
