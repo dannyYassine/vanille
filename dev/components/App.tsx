4956ac5021d3cfcd207ef73fd06178255abb1679
Danny Yassine <dannyyassine@gmail.com>
2023-09-06 00:47:35 +0000

    updates

--- dev/components/App.tsx
+++ dev/components/App.tsx
@@ -1,29 +1,26 @@
-import { BaseView, define, Route } from '../../src/index';
+import { define } from '../../src/index';
 import './Nav';
 import './Main';
 import './Login';
+import { DevView } from './DevView';
 
 @define()
-export class App extends BaseView {
-    globalStylesheet() {
-        return `@import url("${window.location.origin}/assets/css/material-dashboard.css?v=3.1.0");`;
-    }
-
-    render() {
-        return (
-            <section>
-                <v-route path="/login">
-                    <v-login></v-login>
-                </v-route>
-                <v-route start-with="/app">
-                    <div>
-                        <v-nav></v-nav>
-                    </div>
-                    <div style="margin-left: 260px;">
-                        <v-main></v-main>
-                    </div>
-                </v-route>
-            </section>
-        );
-    }
+export class App extends DevView {
+  render() {
+    return (
+      <section>
+        <v-route path="/login">
+          <v-login></v-login>
+        </v-route>
+        <v-route start-with="/app">
+          <div>
+            <v-nav></v-nav>
+          </div>
+          <div style="margin-left: 260px;">
+            <v-main></v-main>
+          </div>
+        </v-route>
+      </section>
+    );
+  }
 }
