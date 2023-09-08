cbd56801022cbdf66824baba86014b761749ae94
Danny Yassine <dannyyassine@gmail.com>
2023-09-08 02:40:51 +0000

    added home page

--- dev/components/App.tsx
+++ dev/components/App.tsx
@@ -3,12 +3,16 @@ import { Nav } from './Nav';
 import { Main } from './Main';
 import { DevView } from './DevView';
 import { Login } from './Login';
+import { Home } from './Home';
 
 @define()
 export class App extends DevView {
   render() {
     return (
       <section>
+        <Route path="/">
+          <Home />
+        </Route>
         <Route path="/login">
           <Login />
         </Route>
