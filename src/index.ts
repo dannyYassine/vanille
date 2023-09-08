7fe571a0e4e80a0e7bd5ade5da707b5e7f2ba7ea
Danny Yassine <dannyyassine@gmail.com>
2023-09-07 22:21:55 -0400

    up

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 // jsx rendering
-export { render, h } from './jsx';
-window.h = h;
+export { render, h } from "./jsx";
 
 // routing components
-export { Route } from './Route';
+export { Route } from "./Route";
 
 // observables
-export { observable } from './Observable';
+export { observable } from "./Observable";
 
 // all decorators
-export * from './decorators';
+export * from "./decorators";
 
 // base class that contains all of the above
-export { BaseView } from './BaseView';
+export { BaseView } from "./BaseView";
